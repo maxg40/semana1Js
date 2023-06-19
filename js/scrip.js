bf58c649e99c3310e83f21c5fa8634b09e13d28e
maxg40 <maxigossa@gmail.com>
2023-06-19 16:48:14 -0300

    ver1

---
+++ js/scrip.js
@@ -0,0 +1,12 @@
+alert ('Bienvenido a nuestro sitio, a continuación se le hara responder un cuestionario');
+
+let nombre = prompt ('¿Cual es tu Nombre?');
+let apellido = prompt ('¿Cual es tu Apellido?');
+
+alert ('Hola' + ' ' + nombre + ' ' + apellido + ' ' + 'ahora completa los siguientes pedidos');
+
+let numero = prompt ('Ingrese un valor numérico para comenzar las operaciones');
+let numero2 = prompt ('Ingrese el segundo valor númerico');
+
+let resultado = numero * numero2 ;
+alert ('Multiplicando tus valores obtuvimos' + ' ' + resultado );
